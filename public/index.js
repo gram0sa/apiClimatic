1ee2bb0f428beb7c759bb21febb8d14ecccc8196
Lucas Gramosa <lucashalliier@gmail.com>
2023-06-07 17:12:50 -0300

    create: icons results

--- public/index.js
+++ public/index.js
@@ -9,14 +9,13 @@ function convertion(val) {
   return (val - 273).toFixed(2);
 }
 
-btn.addEventListener("click", function () {  
+btn.addEventListener("click", function () {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       inputval.value +
       "&appid=" +
       apik
   )
-  
     .then((res) => res.json())
 
     .then((data) => {
@@ -31,20 +30,29 @@ btn.addEventListener("click", function () {
     })
 
     .catch((err) => alert("Please enter a City"));
-    setTimeout(() => { limpaInput() }, 200);
+  setTimeout(() => {
+    limpaInput();
+  }, 200);
+  if (btn) {
+    let descrip = document.querySelector("#img");
+    descrip.classList.remove("hide");
+    let descrip1 = document.querySelector("#img1");
+    descrip1.classList.remove("hide");
+    let descrip2 = document.querySelector("#img2");
+    descrip2.classList.remove("hide");
+  }
 });
 
 inputval.addEventListener("keypress", (e) => {
-  if(e.keyCode === 13) {
+  if (e.keyCode === 13) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         inputval.value +
         "&appid=" +
         apik
     )
-    
       .then((res) => res.json())
-  
+
       .then((data) => {
         var nameval = data["name"];
         var descrip = data["weather"]["0"]["description"];
@@ -55,10 +63,20 @@ inputval.addEventListener("keypress", (e) => {
         description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
         wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
       })
-  
+
       .catch((err) => alert("Please enter a City"));
-      setTimeout(() => { limpaInput() }, 200);
-  };
+    setTimeout(() => {
+      limpaInput();
+    }, 200);
+    if (btn) {
+      let descrip = document.querySelector("#img");
+      descrip.classList.remove("hide");
+      let descrip1 = document.querySelector("#img1");
+      descrip1.classList.remove("hide");
+      let descrip2 = document.querySelector("#img2");
+      descrip2.classList.remove("hide");
+    }
+  }
 });
 
 function limpaInput() {
